c474f063b0fc9152272dacc9b60ac64db53861c8
Lucas Guichard <lucas.guichard@epitech.eu>
2021-05-11 11:19:07 +0200

    Added bootstrap CMake system for UNIX

---
+++ cross_platform/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Cross-platform main" << std::endl;
+    return EXIT_SUCCESS;
+}
