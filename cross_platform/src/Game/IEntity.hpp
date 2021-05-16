acd726eebdd934b93df1557bcd208397ffae92d8
Lucas Guichard <lucas.guichard@epitech.eu>
2021-05-16 09:03:11 +0200

    Added Entity interface

---
+++ cross_platform/src/Game/IEntity.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** IEntity
+*/
+
+#ifndef IENTITY_HPP_
+#define IENTITY_HPP_
+
+#include <iostream>
+
+class IEntity {
+
+    public:
+        /**
+         * @brief Destroy the IEntity object
+         *
+         */
+        virtual ~IEntity() = default;
+};
+
+#endif /* !IENTITY_HPP_ */
