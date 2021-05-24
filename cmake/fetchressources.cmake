3c172a6f04699b0b787bdddc59cbac1dda6c8405
Jonathan José <60665041+Jose-JohnEm@users.noreply.github.com>
2021-05-24 19:56:52 +0200

    Modify the fetchressource module to track new resources repository updates

--- cmake/fetchressources.cmake
+++ cmake/fetchressources.cmake
@@ -1,20 +1,32 @@
 CMAKE_MINIMUM_REQUIRED(VERSION 3.17) # FetchContent is available in 3.11+
 
-message(VERBOSE "Hey")
-
-
 INCLUDE(FetchContent)
-FetchContent_Declare(
-  ressources
-  URL http://51.77.221.38:8080/resources.tar.gz
+
+message("Searching for ressources updates...")
+
+file(DOWNLOAD http://51.77.221.38:8080/last.log .last.log)
+
+execute_process( COMMAND ${CMAKE_COMMAND} -E compare_files 
+  "${CMAKE_CURRENT_SOURCE_DIR}/resources/.last.log"
+  "${CMAKE_CURRENT_SOURCE_DIR}/build/.last.log"
+                 RESULT_VARIABLE compare_result
 )
 
-SET(FETCHCONTENT_QUIET NO)
-FetchContent_GetProperties(ressources)
-if (NOT ressources_POPULATED)
-    FetchContent_Populate(ressources)
-endif ()
 
-file(RENAME ${ressources_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/resources)
+if( compare_result EQUAL 0)
+    message("No update detected...")
+    file(REMOVE ${CMAKE_CURRENT_SOURCE_DIR}/build/.last.log)
+else()
+    message("Need to be updated !")
 
-message(VERBOSE "End")
+    file(DOWNLOAD http://51.77.221.38:8080/resources.tar.gz resources.tar.gz SHOW_PROGRESS)
+    
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E tar xzf resources.tar.gz
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/build
+    )
+    file(REMOVE_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/resources)
+    file(RENAME ${CMAKE_CURRENT_SOURCE_DIR}/build/resources ${CMAKE_CURRENT_SOURCE_DIR}/resources)
+
+    file(RENAME ${CMAKE_CURRENT_SOURCE_DIR}/build/.last.log ${CMAKE_CURRENT_SOURCE_DIR}/resources/.last.log)
+endif()
