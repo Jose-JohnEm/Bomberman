a2bf5f14ffb60f21c7bbfb600050f294e13cb2ff
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-03 20:36:03 +0200

    Renamed MapGenerator class in Map

---
+++ Game/Map/Map.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** Map
+*/
+
+#include "Map.hpp"
+
+Game::Map::Map()
+{
+}
+
+Game::Map::~Map()
+{
+}
---
+++ Game/Map/Map.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** Map
+*/
+
+#ifndef MAP_HPP_
+#define MAP_HPP_
+
+namespace Game
+{
+    class Map
+    {
+        public:
+            /**
+            * @brief Construct the Map object
+            */
+            Map();
+
+            /**
+            * @brief Destroy the Map object
+            */
+            ~Map();
+    };
+
+    #include "Map.inl"
+}
+
+#endif /* !MAP_HPP_ */
