8218cab9eb2387ccc220cbd89e98cc114c7f6dca
CMasterp <christianmiyigbena@gmail.com>
2021-06-05 20:02:06 +0200

    On ingame scene

--- Lib/XRaylib/XRay/sources/InGame.cpp
+++ Lib/XRaylib/XRay/sources/InGame.cpp
@@ -7,9 +7,18 @@
 
 #include "XRay.hpp"
 
-void XRay::displayPlayersPanels(void)
+void XRay::displayPlayersPanels(std::vector<std::pair<size_t, size_t>> &panelPos)
 {
-    ;
+    for (size_t u = 0; u < _allIntegers[2]; u++) {
+        if (_controlsTab[u] == Resources::PLAYSTATIONYELLOW)
+            _resources.at(PLAYSTATIONPANEL)->drawTexture(panelPos[u].first, panelPos[u].second, Raylib::Color::White());
+        if (_controlsTab[u] == Resources::XBOXYELLOW)
+            _resources.at(XBOXPANEL)->drawTexture(panelPos[u].first, panelPos[u].second, Raylib::Color::White());
+        if (_controlsTab[u] == Resources::MOUSEYELLOW)
+            _resources.at(MOUSEPANEL)->drawTexture(panelPos[u].first, panelPos[u].second, Raylib::Color::White());
+        if (_controlsTab[u] == Resources::KEYBOARDYELLOW)
+            _resources.at(KEYBOARDPANEL)->drawTexture(panelPos[u].first, panelPos[u].second, Raylib::Color::White());
+    }
 }
 
 void XRay::displayInGameScene(void)
@@ -17,10 +26,17 @@ void XRay::displayInGameScene(void)
     // Set scene
     _scene = IN_GAME;
 
+    // Lambda for panel pos
+    auto panelLambda = [](size_t a) { return (a <= 2) ? std::vector<std::pair<size_t, size_t>>{{20, 500}, {1500, 500}}
+    : std::vector<std::pair<size_t, size_t>>{{20, 500}, {1500, 500}, {20, 950}, {1500, 950}}; };
+
+    // Position of all Panels in a vector of pair (x, y)
+    static std::vector<std::pair<size_t, size_t>> panelPos = panelLambda(_allIntegers[2]);
+
     // Draw scene
     beginDrawing();
 
-    displayPlayersPanels();
+    displayPlayersPanels(panelPos);
     // Some code
 
     endDrawing();
