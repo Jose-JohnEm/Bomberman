c42ef0797e4ea0cd5f98a877080bbaa646f69067
CMasterp <christianmiyigbena@gmail.com>
2021-06-06 00:02:17 +0200

    In Game Pause

--- Lib/XRaylib/XRay/sources/InGame.cpp
+++ Lib/XRaylib/XRay/sources/InGame.cpp
@@ -26,6 +26,10 @@ void XRay::displayPauseScene(void)
     if (_isPaused) {
         _resources.at(DEPAUSE)->drawTexture(10, 5, Raylib::Color::White());
         _resources.at(PAUSEBACK)->drawTexture(0, 0, Raylib::Color::White());
+        (mouseIsInBox(createBox(460, 445, 460+470, 445+95)) ? _resources.at(RESUMEHOVER) : _resources.at(RESUME))->drawTexture(460, 445, Raylib::Color::White());
+        (mouseIsInBox(createBox(460, 555, 460+470, 555+95)) ? _resources.at(RESTARTHOVER) : _resources.at(Resources::RESTART))->drawTexture(460, 555, Raylib::Color::White());
+        (mouseIsInBox(createBox(460, 665, 460+470, 665+95)) ? _resources.at(SAVEHOVER) : _resources.at(Resources::SAVE))->drawTexture(460, 665, Raylib::Color::White());
+        (mouseIsInBox(createBox(460, 775, 460+470, 775+95)) ? _resources.at(HOMEHOVER) : _resources.at(Resources::HOME))->drawTexture(460, 775, Raylib::Color::White());
     } else
         _resources.at(PAUSE)->drawTexture(10, 5, Raylib::Color::White());
 }
@@ -37,6 +41,11 @@ void XRay::displayInGameScene(void)
 
     // Check if mouse is on button spot
     bool pauseButton = mouseIsInBox(createBox(10, 5, 122, 127)) ? true : false;
+
+    bool resume = mouseIsInBox(createBox(460, 445, 460+470, 445+95)) ? true : false;
+    bool restart = mouseIsInBox(createBox(460, 555, 460+470, 555+95)) ? true : false;
+    bool save = mouseIsInBox(createBox(460, 665, 460+470, 665+95)) ? true : false;
+    bool home = mouseIsInBox(createBox(460, 775, 460+470, 775+95)) ? true : false;
 
     // Lambda for panel pos
     auto panelLambda = [](size_t a) { return (a <= 2) ? std::vector<std::pair<size_t, size_t>>{{20, 500}, {1500, 500}}
@@ -57,8 +66,17 @@ void XRay::displayInGameScene(void)
     displayMouse();
     endDrawing();
 
-
     if (pauseButton && Raylib::Mouse::isButtonPressed(0))
         _isPaused = true;
+    if (resume && Raylib::Mouse::isButtonPressed(0))
+        _isPaused = false;
+    if (restart && Raylib::Mouse::isButtonPressed(0)) {
+        _isPaused = false;
+        displayCinematic("loading", 0, 0);
+        displayInGameScene();
+    }
+    if (save && Raylib::Mouse::isButtonPressed(0));
+    if (home && Raylib::Mouse::isButtonPressed(0))
+        displayMenuScene();
     m_isPaused = _isPaused;
 }
