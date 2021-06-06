d056303bce92768cf7ca038580c96082c38abf45
CMasterp <christianmiyigbena@gmail.com>
2021-06-06 15:19:55 +0200

    Add backupBar and loading scene et click on backup

--- Lib/XRaylib/XRay/sources/LoadGame.cpp
+++ Lib/XRaylib/XRay/sources/LoadGame.cpp
@@ -7,6 +7,15 @@
 
 #include "XRay.hpp"
 
+void XRay::loadThisBackup(const std::string &_pathToBackupFile)
+{
+    // Add deserialization and loading of data
+    // hereeeee (je suis gentil d'où je mets ça ... :/ c'est évident !!! Allez hop hop !!)
+
+    displayCinematic("loading", 0, 0);
+    displayInGameScene();
+}
+
 void XRay::displayLoadGameScene(void)
 {
     // Set scene
@@ -23,6 +32,12 @@ void XRay::displayLoadGameScene(void)
     _resources.at(LOADSCENE)->drawTexture(0, 0);
     Raylib::Text::drawText("BACKUPS", 230, 210, 65, Raylib::Color::Black());
     for (const std::string &backup : _backups) {
+        if (mouseIsInBox(createBox(300, ordinate + 20, 300+(65 * backup.substr(0, backup.find('.')).length()), ordinate + 85)))
+            _resources.at(BACKUPBAR)->drawTexture(295, ordinate + 10);
+        if (mouseIsInBox(createBox(300, ordinate + 20, 300+(65 * backup.substr(0, backup.find('.')).length()), ordinate + 85)) && Raylib::Mouse::isButtonPressed(0)) {
+            loadThisBackup(backup);
+            return ;
+        }
         _resources.at(BRANCH)->drawTexture(150, ordinate);
         Raylib::Text::drawText(backup.substr(0, backup.find('.')), 300, ordinate + 20, 65, Raylib::Color::Black());
         ordinate += 100;
