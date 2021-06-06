32890ed234ecceac693c05f544bcec81cde5b3d7
CMasterp <christianmiyigbena@gmail.com>
2021-06-06 14:01:37 +0200

    Clean Some Lines

--- Lib/XRaylib/XRay/sources/GameMode.cpp
+++ Lib/XRaylib/XRay/sources/GameMode.cpp
@@ -20,12 +20,12 @@ void XRay::displayGameModeScene(void)
     // Draw scene
     beginDrawing();
     if (Raylib::Mouse::getMouseX() < 960)
-        _resources.at(NEWGAME_BG)->drawTexture(0, 0, Raylib::Color::White());
+        _resources.at(NEWGAME_BG)->drawTexture(0, 0);
     else
-        _resources.at(LOADGAME_BG)->drawTexture(0, 0, Raylib::Color::White());
-    (mouseIsInBox(createBox(20, 1000, 280, 1065)) ? _resources.at(BACK_HOVER) : _resources.at(BACK))->drawTexture(20, 1000, Raylib::Color::White());
-    (mouseIsInBox(createBox(180, 500, 738+180, 500+109)) ? _resources.at(NEWGAME_HOVER) : _resources.at(NEWGAME))->drawTexture(180, 500, Raylib::Color::White());
-    (mouseIsInBox(createBox(1140, 500, 1140+738, 500+109)) ? _resources.at(LOADGAME_HOVER) : _resources.at(LOADGAME))->drawTexture(1140, 500, Raylib::Color::White());
+        _resources.at(LOADGAME_BG)->drawTexture(0, 0);
+    (mouseIsInBox(createBox(20, 1000, 280, 1065)) ? _resources.at(BACK_HOVER) : _resources.at(BACK))->drawTexture(20, 1000);
+    (mouseIsInBox(createBox(180, 500, 738+180, 500+109)) ? _resources.at(NEWGAME_HOVER) : _resources.at(NEWGAME))->drawTexture(180, 500);
+    (mouseIsInBox(createBox(1140, 500, 1140+738, 500+109)) ? _resources.at(LOADGAME_HOVER) : _resources.at(LOADGAME))->drawTexture(1140, 500);
     displayMouse();
     endDrawing();
 
