bd62e0043dd34e82da45a1121ec540f43c34d5ae
CMasterp <christianmiyigbena@gmail.com>
2021-06-06 13:23:10 +0200

    Add Back Button Update on settings scene

--- Lib/XRaylib/XRay/sources/Settings.cpp
+++ Lib/XRaylib/XRay/sources/Settings.cpp
@@ -22,6 +22,8 @@ void XRay::displaySettingsScene(void)
     endDrawing();
 
     // Go to another scene according to mouse position
-    if (goBack && Raylib::Mouse::isButtonPressed(0))
-        displayMenuScene();
+    if (goBack && Raylib::Mouse::isButtonPressed(0)) {
+        (this->*_scenesBack[_scene])();
+        _scenesBack[SETTINGS] = _scenesBackBackup[SETTINGS];
+    }
 }
