870a1ef492f1e41c5959166a1fdf74ba3d2e5f61
CMasterp <christianmiyigbena@gmail.com>
2021-06-07 13:30:39 +0200

    ADD Bomberman.cpp Bomberman.hpp

---
+++ Game/includes/Bomberman.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-prince.miyigbena
+** File description:
+** Bomberman
+*/
+
+#ifndef BOMBERMAN_HPP_
+#define BOMBERMAN_HPP_
+
+#include <vector>
+#include <istream>
+#include <algorithm>
+
+namespace Game
+{
+    class Bomberman {
+        public:
+            Bomberman();
+            ~Bomberman();
+
+        private:
+    };
+}
+
+#endif /* !BOMBERMAN_HPP_ */
---
+++ Game/sources/Bomberman.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-prince.miyigbena
+** File description:
+** Bomberman
+*/
+
+#include "Bomberman.hpp"
+
+Game::Bomberman::Bomberman()
+{
+}
+
+Game::Bomberman::~Bomberman()
+{
+}
