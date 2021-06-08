e8f223c24badd60c954ecdc972901ea213135036
KinoFool <nicolas.mary@epitech.eu>
2021-06-08 15:29:18 +0200

    Re add setAudioResources function

--- Lib/XRaylib/XRay/includes/XRay.hpp
+++ Lib/XRaylib/XRay/includes/XRay.hpp
@@ -290,6 +290,11 @@ class XRay : public IGraphical {
         void setResources(void);
 
         /**
+         * @brief Set the Audio
+         */
+        void setAudioResources(void);
+
+        /**
          * @brief Display Game Settings
          */
         void displayGameSettings(void);
