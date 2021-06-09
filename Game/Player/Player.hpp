75610a3fa04ded9c024f9616bc64aa8103d5287c
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-09 17:50:53 +0200

    Added Player setters and getters

--- Game/Player/Player.hpp
+++ Game/Player/Player.hpp
@@ -35,12 +35,82 @@ namespace Game
             */
             ~Player();
 
+            /**
+             * @brief Get the Name of the Player
+             *
+             * @return A string
+             */
+            std::string getName(void) const;
+
+            /**
+             * @brief Get the Positions of the Player
+             *
+             * @return A pair
+             */
+            std::pair<float, float> getPositions(void) const;
+
+            /**
+             * @brief Get Broken Wall by the Player
+             *
+             * @return A const a Pair
+             */
+            size_t getBrokenWalls(void) const;
+
+            /**
+             * @brief Get Broken Wall by the Player
+             *
+             * @return A const a size_t
+             */
+            size_t getKills(void) const;
+
+            /**
+             * @brief Get PowerUps gotten by the Player
+             *
+             * @return A const a Array
+             */
+            std::array<size_t, 7> getPowerUps(void) const;
+
+            /**
+             * @brief Set the Name object
+             *
+             * @param name A const reference to a string
+             */
+            void setName(const std::string &name);
+
+            /**
+             * @brief Set the Player positions
+             *
+             * @param positions A const reference to a pair of floats
+             */
+            void setPositions(const std::pair<float, float> &positions);
+
+            /**
+             * @brief Set the broken walls number
+             *
+             * @param brokenWalls A const reference to a size_t
+             */
+            void setBrokenWalls(const size_t &brokenWalls);
+
+            /**
+             * @brief Set the kills number
+             *
+             * @param kills A const reference to a size_t
+             */
+            void setKills(const size_t &kills);
+
+            /**
+             * @brief Set the power ups object
+             *
+             * @param powerUps A const reference to an array
+             */
+            void setPowerUps(const std::array<size_t, 5> &powerUps);
+
         private:
             std::string _name; // Name of the player entitiy
             std::pair<float, float> _positions; // A pair of float for player positions (x, y)
-            int _brokenWalls; // Amount of broken walls
-            int _kills; // Amount of kills
-            std::array<size_t, 5> _powerups; // Amount of powerups
+            size_t _brokenWalls; // Amount of broken walls
+            size_t _kills; // Amount of kills
+            std::array<size_t, 5> _powerUps; // Amount of powerups
     };
 
     #include "Player.inl"
--- Game/Player/Player.inl
+++ Game/Player/Player.inl
@@ -4,3 +4,53 @@
 ** File description:
 ** Player
 */
+
+inline std::string Player::getName() const
+{
+    return _name;
+}
+
+inline std::pair<float, float> Player::getPositions() const
+{
+    return _positions;
+}
+
+inline size_t Player::getBrokenWalls() const
+{
+    return _brokenWalls;
+}
+
+inline size_t Player::getKills() const
+{
+    return _kills;
+}
+
+inline std::array<size_t, 7> getPowerUps() const
+{
+    return _powerUps;
+}
+
+inline void Player::setName(const std::string &name)
+{
+    _name = name;
+}
+
+inline void Player::setPositions(const std::pair<float, float> &positions)
+{
+    _positions = positions;
+}
+
+inline void Player::setBrokenWalls(const size_t &brokenWalls)
+{
+    _brokenWalls = brokenWalls
+}
+
+inline void Player::setKills(const size_t &kills)
+{
+    _kills = kills;
+}
+
+inline void setPowerUps(const std::array<size_t, 5> &powerUps)
+{
+    _powerUps = powerUps;
+}
