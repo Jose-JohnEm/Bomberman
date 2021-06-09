ec1d362a1f9fb9bdd45b6e9e9eaf06c5c6c4be4d
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-09 17:21:45 +0200

    Added settings enumeration

---
+++ Game/Save/Save.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** Save
+*/
+
+#include "Save.hpp"
+
+Game::Save::Save()
+{
+}
+
+Game::Save::~Save()
+{
+}
