764b0e1592908b9e4a31aeb392babfb9615b4b77
CMasterp <christianmiyigbena@gmail.com>
2021-06-11 10:41:25 +0200

    Add Mouse Player Input Class

---
+++ Lib/XRaylib/XRay/sources/MousePlayerInput.cpp
@@ -0,0 +1,110 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** MousePlayerInput
+*/
+
+#include "MousePlayerInput.hpp"
+
+/*
+    MOUSE_BUTTON_LEFT    = 0,
+    MOUSE_BUTTON_RIGHT   = 1,
+    MOUSE_BUTTON_MIDDLE  = 2,
+    MOUSE_BUTTON_SIDE    = 3,
+    MOUSE_BUTTON_EXTRA   = 4,
+    MOUSE_BUTTON_FORWARD = 5,
+    MOUSE_BUTTON_BACK    = 6,
+
+1600 40
+250 250
+        1725
+          |
+       _______
+      |o     o|
+165 - |   .   |
+      |o_____o|
+*/
+
+MousePlayerInput::MousePlayerInput()
+{
+}
+
+MousePlayerInput::~MousePlayerInput()
+{
+}
+
+bool MousePlayerInput::shouldGoToEast(void) const
+{
+    bool mousePos;
+    int x = (int)Raylib::Mouse::getMouseX() - 1725;
+    int y = (int)Raylib::Mouse::getMouseY() - 165;
+
+    if ((Raylib::Mouse::getMouseX() < 1600 || Raylib::Mouse::getMouseX() > 1850) || (Raylib::Mouse::getMouseY() < 40 || Raylib::Mouse::getMouseY() > 290))
+        mousePos = false;
+    else if (y >= 0)
+        mousePos = -(x + y) >= 0 ? true : false;
+    else
+        mousePos = (y - x) >= 0 ? true : false;
+    return Raylib::Mouse::isButtonPressed(2) && mousePos;
+}
+
+bool MousePlayerInput::shouldGoToWest(void) const
+{
+    bool mousePos;
+    int x = (int)Raylib::Mouse::getMouseX() - 1725;
+    int y = (int)Raylib::Mouse::getMouseY() - 165;
+
+    if ((Raylib::Mouse::getMouseX() < 1600 || Raylib::Mouse::getMouseX() > 1850) || (Raylib::Mouse::getMouseY() < 40 || Raylib::Mouse::getMouseY() > 290))
+        mousePos = false;
+    else if (y >= 0)
+        mousePos = (x - y) >= 0 ? true : false;
+    else
+        mousePos = (x + y) >= 0 ? true : false;
+    return Raylib::Mouse::isButtonPressed(2) && mousePos;
+}
+
+bool MousePlayerInput::shouldGoToNorth(void) const
+{
+    bool mousePos;
+    int x = (int)Raylib::Mouse::getMouseX() - 1725;
+    int y = (int)Raylib::Mouse::getMouseY() - 165;
+
+    if ((Raylib::Mouse::getMouseX() < 1600 || Raylib::Mouse::getMouseX() > 1850) || (Raylib::Mouse::getMouseY() < 40 || Raylib::Mouse::getMouseY() > 290))
+        mousePos = false;
+    else if (x >= 0)
+        mousePos = (y - x) >= 0 ? true : false;
+    else
+        mousePos = (x + y) >= 0 ? true : false;
+    return Raylib::Mouse::isButtonPressed(2) && mousePos;
+}
+
+bool MousePlayerInput::shouldGoToSouth(void) const
+{
+    bool mousePos;
+    int x = (int)Raylib::Mouse::getMouseX() - 1725;
+    int y = (int)Raylib::Mouse::getMouseY() - 165;
+
+    if ((Raylib::Mouse::getMouseX() < 1600 || Raylib::Mouse::getMouseX() > 1850) || (Raylib::Mouse::getMouseY() < 40 || Raylib::Mouse::getMouseY() > 290))
+        mousePos = false;
+    else if (x >= 0)
+        mousePos = -(x + y) >= 0 ? true : false;
+    else
+        mousePos = (x - y) >= 0 ? true : false;
+    return Raylib::Mouse::isButtonPressed(2) && mousePos;
+}
+
+bool MousePlayerInput::shouldChangeToPrev(void) const
+{
+    return Raylib::Mouse::getMouseWheelMove() > 0;
+}
+
+bool MousePlayerInput::shouldChangeToNext(void) const
+{
+    return Raylib::Mouse::getMouseWheelMove() < 0;
+}
+
+bool MousePlayerInput::shouldSimulateAClick(void) const
+{
+    return Raylib::Mouse::isButtonPressed(1);
+}
