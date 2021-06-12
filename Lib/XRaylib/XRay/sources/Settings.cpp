f48a4052a5a4422db01424cb60c226cdf3437932
KinoFool <nicolas.mary@epitech.eu>
2021-06-12 10:32:34 +0200

    Reposition of all the button on setting scene

--- Lib/XRaylib/XRay/sources/Settings.cpp
+++ Lib/XRaylib/XRay/sources/Settings.cpp
@@ -17,13 +17,13 @@ void XRay::displaySettingsScene(void)
 
     // Check if mouse is on button spot
     bool goBack = mouseIsInBox(createBox(20, 1000, 280, 1065)) ? true : false;
-    bool onFullscreen = mouseIsInBox(createBox(1500, 200, 1725, 258)) ? true : false;
-    bool onMasterVolumeUp = mouseIsInBox(createBox(1700, 300, 1764, 364)) ? true : false;
-    bool onMasterVolumeDown = mouseIsInBox(createBox(1500, 300, 1564, 364)) ? true : false;
-    bool onMusicVolumeUp = mouseIsInBox(createBox(1700, 400, 1764, 464)) ? true : false;
-    bool onMusicVolumeDown = mouseIsInBox(createBox(1500, 400, 1564, 464)) ? true : false;
-    bool onSfxVolumeUp = mouseIsInBox(createBox(1700, 500, 1764, 564)) ? true : false;
-    bool onSfxVolumeDown = mouseIsInBox(createBox(1500, 500, 1564, 564)) ? true : false;
+    bool onFullscreen = mouseIsInBox(createBox(1200, 210, 1425, 268)) ? true : false;
+    bool onMasterVolumeDown = mouseIsInBox(createBox(1185, 300, 1249, 364)) ? true : false;
+    bool onMasterVolumeUp = mouseIsInBox(createBox(1390, 300, 1454, 364)) ? true : false;
+    bool onMusicVolumeDown = mouseIsInBox(createBox(1185, 390, 1249, 454)) ? true : false;
+    bool onMusicVolumeUp = mouseIsInBox(createBox(1390, 390, 1764, 454)) ? true : false;
+    bool onSfxVolumeDown = mouseIsInBox(createBox(1185, 475, 1249, 539)) ? true : false;
+    bool onSfxVolumeUp = mouseIsInBox(createBox(1390, 475, 1764, 539)) ? true : false;
 
     // On / off buton for fullscreen button
     std::shared_ptr<Raylib::Texture> fullscreenButton = _resources.at(Raylib::Window::isWindowFullscreen() ? ON : OFF);
@@ -35,26 +35,26 @@ void XRay::displaySettingsScene(void)
     _resources.at(SETTING_BG)->drawTexture(0, 0, Raylib::Color::White());
 
     // Fullscreen
-    fullscreenButton->drawTexture(1500, 200, Raylib::Color::White());
-    Raylib::Text::drawText("Fullscreen :", 1200, 205, 48, Raylib::Color::Yellow());
+    Raylib::Text::drawText("Fullscreen :", 765, 210, 48, Raylib::Color::Yellow());
+    fullscreenButton->drawTexture(1200, 210, Raylib::Color::White());
 
     // Master Volume
-    Raylib::Text::drawText("Global volume :", 1100, 305, 48, Raylib::Color::Yellow());
-    _resources.at(VOLUME_DOWN)->drawTexture(1500, 300, Raylib::Color::White());
-    Raylib::Text::drawText(std::to_string((int)masterVolume), 1600, 305, 48, Raylib::Color::Yellow());
-    _resources.at(VOLUME_UP)->drawTexture(1700, 300, Raylib::Color::White());
+    Raylib::Text::drawText("Global volume :", 765, 310, 48, Raylib::Color::Yellow());
+    _resources.at(VOLUME_DOWN)->drawTexture(1185, 300, Raylib::Color::White());
+    Raylib::Text::drawText(std::to_string((int)masterVolume), 1290, 310, 48, Raylib::Color::Yellow());
+    _resources.at(VOLUME_UP)->drawTexture(1390, 300, Raylib::Color::White());
 
     // Music volume
-    Raylib::Text::drawText("Music volume :", 1100, 405, 48, Raylib::Color::Yellow());
-    _resources.at(VOLUME_DOWN)->drawTexture(1500, 400, Raylib::Color::White());
-    Raylib::Text::drawText(std::to_string((int)musicVolume), 1600, 405, 48, Raylib::Color::Yellow());
-    _resources.at(VOLUME_UP)->drawTexture(1700, 400, Raylib::Color::White());
+    Raylib::Text::drawText("Music volume :", 765, 400, 48, Raylib::Color::Yellow());
+    _resources.at(VOLUME_DOWN)->drawTexture(1185, 390, Raylib::Color::White());
+    Raylib::Text::drawText(std::to_string((int)musicVolume), 1290, 400, 48, Raylib::Color::Yellow());
+    _resources.at(VOLUME_UP)->drawTexture(1390, 390, Raylib::Color::White());
 
     // Sfx volume
-    Raylib::Text::drawText("Sfx volume :", 1100, 505, 48, Raylib::Color::Yellow());
-    _resources.at(VOLUME_DOWN)->drawTexture(1500, 500, Raylib::Color::White());
-    Raylib::Text::drawText(std::to_string((int)sfxVolume), 1600, 505, 48, Raylib::Color::Yellow());
-    _resources.at(VOLUME_UP)->drawTexture(1700, 500, Raylib::Color::White());
+    Raylib::Text::drawText("Sfx volume :", 765, 490, 48, Raylib::Color::Yellow());
+    _resources.at(VOLUME_DOWN)->drawTexture(1185, 475, Raylib::Color::White());
+    Raylib::Text::drawText(std::to_string((int)sfxVolume), 1290, 490, 48, Raylib::Color::Yellow());
+    _resources.at(VOLUME_UP)->drawTexture(1390, 475, Raylib::Color::White());
 
     // Back button
     (mouseIsInBox(createBox(20, 1000, 280, 1065)) ? _resources.at(BACK_HOVER) : _resources.at(BACK))->drawTexture(20, 1000, Raylib::Color::White());
