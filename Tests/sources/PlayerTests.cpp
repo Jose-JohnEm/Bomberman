311810c994e9f8b1500ca503afee9bfeac1e9d15
Mehdi <mehdi.zehri@epitech.eu>
2021-06-12 13:55:14 +0200

    Added new test to PlayerTests file

--- Tests/sources/PlayerTests.cpp
+++ Tests/sources/PlayerTests.cpp
@@ -8,6 +8,23 @@
 #include <gtest/gtest.h>
 #include "../Game/Player/Player.hpp"
 
+TEST(PlayerTests, copy_const)
+{
+    Game::Player player("PlayerName", {30, 40}, 2, 200, {1, 0, 0, 1, 0});
+    std::array<size_t, 5> powerups = player.getPowerUps();
+
+    EXPECT_EQ(player.getName(), "PlayerName");
+    EXPECT_EQ(player.getPositions().first, 30);
+    EXPECT_EQ(player.getPositions().second, 40);
+    EXPECT_EQ(player.getKills(), 2);
+    EXPECT_EQ(player.getBrokenWalls(), 200);
+    EXPECT_EQ(powerups[P_SKATE], 1);
+    EXPECT_EQ(powerups[P_BOMB], 0);
+    EXPECT_EQ(powerups[P_PASS], 0);
+    EXPECT_EQ(powerups[P_FIRE], 1);
+    EXPECT_EQ(powerups[P_LIFE], 0);
+}
+
 TEST(PlayerTests, name)
 {
     Game::Player player;
@@ -16,13 +33,13 @@ TEST(PlayerTests, name)
     EXPECT_EQ(player.getName(), "PlayerName");
 }
 
-TEST(PlayerTests, brokenWall)
+TEST(PlayerTests, positions)
 {
     Game::Player player;
 
-    player.setBrokenWalls(200);
-    EXPECT_EQ(200, player.getBrokenWalls());
-
+    player.setPositions({30, 40});
+    EXPECT_EQ(player.getPositions().first, 30);
+    EXPECT_EQ(player.getPositions().second, 40);
 }
 
 TEST(PlayerTests, kills)
@@ -33,13 +50,12 @@ TEST(PlayerTests, kills)
     EXPECT_EQ(player.getKills(), 2);
 }
 
-TEST(PlayerTests, positions)
+TEST(PlayerTests, brokenWall)
 {
     Game::Player player;
 
-    player.setPositions({30, 40});
-    EXPECT_EQ(player.getPositions().first, 30);
-    EXPECT_EQ(player.getPositions().second, 40);
+    player.setBrokenWalls(200);
+    EXPECT_EQ(player.getBrokenWalls(), 200);
 }
 
 TEST(PlayerTests, powerUps)
