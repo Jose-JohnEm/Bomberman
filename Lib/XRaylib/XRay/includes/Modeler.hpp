8035ef2709e38a214319cbdd3cd62bf421a5d0d5
Jonathan José <60665041+Jose-JohnEm@users.noreply.github.com>
2021-06-13 15:21:38 +0200

    Add Modeler Class to animate models more easier

---
+++ Lib/XRaylib/XRay/includes/Modeler.hpp
@@ -0,0 +1,59 @@
+/*
+** EPITECH PROJECT, 2020
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** Modeler.hpp
+*/
+
+#ifndef MODELER_HPP_
+#define MODELER_HPP_
+
+#include <string>
+#include <vector>
+#include "raylib.h"
+
+#include "Clock.hpp"
+
+#define FRAMETIME 0.03f
+
+struct Animator
+{
+    const std::string WALK = "null";
+    const std::string BOMB = "null";
+    const std::string EMOTE = "null";
+};
+
+enum AnimSequence {
+    WALK,
+    BOMB,
+    EMOTE
+};
+
+class Modeler
+{
+    public:
+        Modeler(const std::string &obj_path, const std::string &texture_path, const Animator &animation_path);
+        ~Modeler();
+
+        void makeWalk();
+        void makeBomb();
+        void makeEmote();
+
+        void update();
+        Model &operator->();
+
+    private:
+
+        bool isThereAnimationsPath(const Animator &animation_path) const;
+
+        Model _model;
+        std::vector<ModelAnimation> _animations;
+
+        const bool _isAnimated;
+        int _frame;
+
+        Clock clock;
+        int _currentAnimation;
+};
+
+#endif
---
+++ Lib/XRaylib/XRay/sources/Modeler.cpp
@@ -0,0 +1,67 @@
+/*
+** EPITECH PROJECT, 2020
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** Modeler.cpp
+*/
+
+#include "Modeler.hpp"
+
+Modeler::Modeler(const std::string &obj_path, const std::string &texture_path, const Animator &animation_path)
+: _model(LoadModel(texture_path.c_str())),
+_isAnimated(isThereAnimationsPath(animation_path)),
+_currentAnimation(WALK)
+{
+    int fake_counter = 0;
+
+    if (_isAnimated)
+    {
+        _animations.push_back(LoadModelAnimations(animation_path.WALK.c_str(), &fake_counter)[0]);
+        _animations.push_back(LoadModelAnimations(animation_path.BOMB.c_str(), &fake_counter)[0]);
+        _animations.push_back(LoadModelAnimations(animation_path.EMOTE.c_str(), &fake_counter)[0]);
+    }
+
+    SetMaterialTexture(&_model.materials[0], MAP_DIFFUSE, LoadTexture(texture_path.c_str()));
+}
+
+Modeler::~Modeler()
+{
+
+}
+
+bool Modeler::isThereAnimationsPath(const Animator &animation_path) const
+{
+    if (animation_path.EMOTE == "null" || animation_path.BOMB == "null" || animation_path.WALK == "null")
+        return false;
+    return true;
+}
+
+void Modeler::update()
+{
+    if (clock.doesTimeElapsed(FRAMETIME)) {
+        _frame++;
+        if (_frame >= _animations[_currentAnimation].frameCount)
+            _frame = 0;
+        UpdateModelAnimation(_model, _animations[_currentAnimation], _frame);
+    }
+}
+
+void Modeler::makeWalk()
+{
+    _currentAnimation = WALK;
+}
+
+void Modeler::makeBomb()
+{
+    _currentAnimation = BOMB;
+}
+
+void Modeler::makeEmote()
+{
+    _currentAnimation = EMOTE;
+}
+
+Model &Modeler::operator->()
+{
+    return _model;
+}
