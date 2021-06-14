74f02df1c7e9dae49007e97ae2b84ad8e1c7de18
CMasterp <christianmiyigbena@gmail.com>
2021-06-14 18:42:33 +0200

    Remove Trailing Space

--- Lib/XRaylib/XRay/includes/XRay.hpp
+++ Lib/XRaylib/XRay/includes/XRay.hpp
@@ -472,9 +472,10 @@ class XRay : public IGraphical {
          * @param y A size_t
          */
         void drawPlayersHead(size_t i, size_t x, size_t y);
+
         /**
          * @brief Get players Data to the Game
-         * 
+         *
          * @return std::vector<PlayerData> represents all the players data
          */
         std::vector<CharDictionary> getPlayersData() override;
