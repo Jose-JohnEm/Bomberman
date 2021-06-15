de8db6a7b819e8cfaaeff0352325c6f002ff968c
Jonathan José <60665041+Jose-JohnEm@users.noreply.github.com>
2021-06-15 10:33:25 +0200

    Now the texture selected on map selector is the correct dislayed one in game

--- Game/MotionlessEntity/MotionlessEntity.cpp
+++ Game/MotionlessEntity/MotionlessEntity.cpp
@@ -11,25 +11,25 @@ Game::SolidWall::SolidWall(Raylib::Vector3 positions, size_t mapType)
 {
     _positions = positions;
     _mapType = mapType;
-    _textures.push_back(*(new Raylib::Texture("resources/map/Wood/wall.png")));
-    _textures.push_back(*(new Raylib::Texture("resources/map/Snow/wall.png")));
     _textures.push_back(*(new Raylib::Texture("resources/map/Iron/wall.png")));
+    _textures.push_back(*(new Raylib::Texture("resources/map/Snow/wall.png")));
+    _textures.push_back(*(new Raylib::Texture("resources/map/Wood/wall.png")));
 }
 
 Game::BreakableWall::BreakableWall(Raylib::Vector3 positions, size_t mapType)
 {
     _positions = positions;
     _mapType = mapType;
-    _textures.push_back(*(new Raylib::Texture("resources/map/Wood/box.png")));
-    _textures.push_back(*(new Raylib::Texture("resources/map/Snow/box.png")));
     _textures.push_back(*(new Raylib::Texture("resources/map/Iron/box.png")));
+    _textures.push_back(*(new Raylib::Texture("resources/map/Snow/box.png")));
+    _textures.push_back(*(new Raylib::Texture("resources/map/Wood/box.png")));
 }
 
 Game::Floor::Floor(Raylib::Vector3 positions, size_t mapType)
 {
     _positions = positions;
     _mapType = mapType;
-    _textures.push_back(*(new Raylib::Texture("resources/map/Wood/floor.png")));
-    _textures.push_back(*(new Raylib::Texture("resources/map/Snow/floor.png")));
     _textures.push_back(*(new Raylib::Texture("resources/map/Iron/floor.png")));
+    _textures.push_back(*(new Raylib::Texture("resources/map/Snow/floor.png")));
+    _textures.push_back(*(new Raylib::Texture("resources/map/Wood/floor.png")));
 }
