fd21f45f1f04136ef8adca451832f2bf23add1b3
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-17 19:06:11 +0200

    Added main method of Astar algo

--- Game/AI/Astar.cpp
+++ Game/AI/Astar.cpp
@@ -31,3 +31,23 @@ Game::Astar::Astar(const std::vector<std::string> &map, const Point &start, cons
     )
 {
 }
+
+Game::Node::Node(const Point &positions, const Point &parentPositions, const int &g, const int &h)
+    : _positions(positions), _parentPositions(parentPositions), _g(g), _h(h)
+{
+}
+
+bool Game::Astar::targetIsFound(void)
+{
+    Node start(_start, 0, 0, calculateHValue(_start));
+
+    _open.push_back(start);
+    while (!_open.empty())
+    {
+        // Load the open list head
+        Node openHead = _open.front();
+        // Remove the open list head
+        _open.pop_front();
+    }
+    return false;
+}
