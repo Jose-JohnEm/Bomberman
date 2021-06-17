8c68b7d633ed1ffcc2ee41732ef6550817419d64
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-17 10:46:56 +0200

    Added AI folder with Point class

---
+++ Game/AI/Astar.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** Astar
+*/
+
+#include "Astar.hpp"
+
+Game::Point::Point(const int &x, const int &y)
+    : _x(x), _y(y)
+{
+}
