26085498cb1441c74e11cb0924210a21f9774a6d
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-17 10:58:42 +0200

    Added Astar constructor

--- Game/AI/Astar.cpp
+++ Game/AI/Astar.cpp
@@ -8,6 +8,11 @@
 #include "Astar.hpp"
 
 Game::Point::Point(const int &x, const int &y)
-    : _x(x), _y(y)
+    : _x{x}, _y{y}
+{
+}
+
+Game::Astar::Astar(const std::vector<std::string> &map, const Point &start, const Point &target)
+    : _map(map), _start(start), _target(target)
 {
 }
