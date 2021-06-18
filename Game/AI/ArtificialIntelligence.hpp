3d7ff0c4e718bea0d7f3ecf9f74b2d427fba0f5f
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-18 12:42:50 +0200

    Added ArtificialIntelligence copy constructor

--- Game/AI/ArtificialIntelligence.hpp
+++ Game/AI/ArtificialIntelligence.hpp
@@ -9,6 +9,8 @@
 #define ARTIFICIALINTELLIGENCE_HPP_
 
 #include "../Player/Player.hpp"
+#include "../Bomberman/Bomberman.hpp"
+#include "Astar.hpp"
 
 namespace Game
 {
@@ -24,10 +26,24 @@ namespace Game
              */
             ArtificialIntelligence(const std::vector<AI> &AIs, const std::vector<Human> &humans, const std::vector<std::string> &map);
 
+            /**
+             * @brief Run the AI algorithm
+             */
+            void run(void) const;
+
         private:
             std::vector<AI> _AIs; // A vector of AI objects
             std::vector<Human> _humans; // A vector of Human objects
             std::vector<std::string> _map; // The game map
+
+            /**
+             * @brief Calcul the heuristic distance between two points (Euclidean Distance)
+             *
+             * @param start Starting point
+             * @param end Ending point
+             * @return float - The Euclidean distance between the 2 points
+             */
+            float calculateDistance(const Point &start, const Point &end) const;
     };
 
     #include "ArtificialIntelligence.inl"
--- Game/AI/ArtificialIntelligence.inl
+++ Game/AI/ArtificialIntelligence.inl
@@ -4,3 +4,8 @@
 ** File description:
 ** ArtificialIntelligence
 */
+
+inline float Game::ArtificialIntelligence::calculateDistance(const Point &start, const Point &end) const
+{
+    return std::sqrt(std::pow(end._x - start._x, 2) + std::pow(end._y - start._y, 2));
+}
