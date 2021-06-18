1ed2c4ec1741b60d10cd0cd9936d10d027959203
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-18 11:12:30 +0200

    Added AI files

---
+++ Game/AI/AI.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-lucas.guichard
+** File description:
+** AI
+*/
+
+#ifndef AI_HPP_
+#define AI_HPP_
+
+namespace Game
+{
+    class AI
+    {
+    };
+
+    #include "AI.inl"
+}
+
+#endif /* !AI_HPP_ */
