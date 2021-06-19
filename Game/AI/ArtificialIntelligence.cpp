6a3b60a6d7dac583dad07cc58c7b928aec9c9726
Lucas Guichard <lucas.guichard@epitech.eu>
2021-06-19 10:58:45 +0200

    Declared human, AI and target positions above the function code

--- Game/AI/ArtificialIntelligence.cpp
+++ Game/AI/ArtificialIntelligence.cpp
@@ -20,19 +20,26 @@ void Game::ArtificialIntelligence::run(void) const
 {
     int smallestDistance = _map.size() * 2;
     Raylib::Vector3 targetPositions = {0, 0, 0};
+    std::list<Point> path;
+    std::pair<int, int> point;
+    int xAI = 0, yAI = 0, xHuman = 0, yHuman = 0, xTarget = 0, yTarget = 0, currentDistance = 0;
 
     for (const Game::AI &AI : _AIs)
     {
         // Get AI positions
         Raylib::Vector3 AIPositions = AI.getPositions();
+        xAI = static_cast<int>(std::round(AIPositions.x));
+        yAI = static_cast<int>(std::round(AIPositions.y));
 
         for (const Game::Human &human : _humans)
         {
             // Get human positions
             Raylib::Vector3 humanPositions = human.getPositions();
+            xHuman = static_cast<int>(std::round(humanPositions.x));
+            yHuman = static_cast<int>(std::round(humanPositions.y));
 
             // Calcul euclidean distance
-            int currentDistance = calculateDistance(Point(AIPositions.x, AIPositions.y), Point(humanPositions.x, humanPositions.y));
+            currentDistance = calculateDistance(Point(xAI, yAI), Point(xHuman, yHuman));
 
             // Set the smallest distance
             smallestDistance = (currentDistance < smallestDistance) ? currentDistance : smallestDistance;
@@ -41,24 +48,29 @@ void Game::ArtificialIntelligence::run(void) const
             if (smallestDistance == currentDistance)
             {
                 targetPositions = humanPositions;
+                xTarget = static_cast<int>(std::round(targetPositions.x));
+                yTarget = static_cast<int>(std::round(targetPositions.y));
             }
         }
 
         // Run Astar algorithm
-        Astar astar(_map, Point(AIPositions.x, AIPositions.y), Point(targetPositions.x, targetPositions.y));
+        Astar astar(_map, Point(xAI, yAI), Point(xTarget, yTarget));
 
         if (astar.targetIsFound())
         {
             // Get the path finding
-            std::list<Point> path;
             astar.fillPath(path);
 
             // Get the next direction
-            std::pair<int, int> point = std::make_pair((*path.begin() + 1)._x - AIPositions.x,  (*path.begin() + 1)._y - AIPositions.y);
+            point = std::make_pair((*path.begin() + 1)._x - xAI,  (*path.begin() + 1)._y - yAI);
+            std::cout << "AI n°" << AI.getID() << " " << convertPointIntoDirection(point) << std::endl;
 
             // Move the AI to this direction
             // Bomberman game;
             // game.doPlayerAction(AI.getID(), convertPointIntoDirection(point));
+
+            // Clear the path
+            path.clear();
         }
         else
         {
