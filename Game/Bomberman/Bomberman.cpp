2d3533ce32decb534d04227a0ef45d673e6424e8
Jonathan José <60665041+Jose-JohnEm@users.noreply.github.com>
2021-06-19 17:30:21 +0200

    Update Bomberman.cpp

--- Game/Bomberman/Bomberman.cpp
+++ Game/Bomberman/Bomberman.cpp
@@ -208,16 +208,19 @@ void Game::Bomberman::bombExplosion(Game::Bomb &bomb, const size_t &index)
 
 void Game::Bomberman::handleIfPlayerIsNearAnItem(Player &player)
 {
+    size_t index = 0;
+
     for (auto &entity : _entities)
     {
         if (entity->getType() == "Item" && CheckCollisionSpheres(player.getPositions().getCStruct(), 0.3, entity->getPositions().getCStruct(), 0.3))
         {
             dynamic_cast<Game::Powerups *>(entity.get())->applyPowerupTo(player);
-            sound.play();
+            //sound.play();
             std::cout << "<<<< Got the..." << std::endl;
             _entities.erase(_entities.begin() + index);
             std::cout << "<<<< Item" << std::endl;
         }
+        index++;
     }
 }
 
